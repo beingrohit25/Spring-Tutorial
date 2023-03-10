9739524b6a97d4205b7bcc09f99a1414a13e40f1
Rohit Parihar <rohitparih@gmail.com>
2023-03-11 00:12:56 +0530

    Friday 11-march-2023

--- validations/src/main/java/com/validations/validations/Implementation/userImplementation.java
+++ validations/src/main/java/com/validations/validations/Implementation/userImplementation.java
@@ -4,9 +4,15 @@ import com.validations.validations.Entity.User;
 import com.validations.validations.Payload.userClone;
 import com.validations.validations.Repository.userRepository;
 import com.validations.validations.Service.userService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class userImplementation implements userService {
 
 //    @Autowired
@@ -14,18 +20,45 @@ public class userImplementation implements userService {
 
     private final userRepository userRepository;
 
-    //Constructor Injection
-    public userImplementation(userRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
     @Override
     public User createUser(userClone userClone) {
-        return null;
+
+        log.info("Entering in Service layer for createUser");
+        log.warn("Converting clone to Entity");
+
+        User user = cloneToEntity(userClone);
+        user.setDateCreated(LocalDate.now());
+
+        log.warn("Sending data to Repository");
+
+        User savedUser = userRepository.save(user);
+
+        log.warn("Return createUser from service layer");
+
+        return savedUser;
     }
 
     @Override
     public User getById(Long userId) {
         return null;
+    }
+
+    /**
+     * Method 1
+     * @param userClone
+     * @return
+     */
+    public User cloneToEntity(userClone userClone){
+        User user = new User();
+        user.setName(userClone.getName());
+        user.setCity(userClone.getCity());
+        user.setEmail(userClone.getEmail());
+        user.setAge(userClone.getAge());
+        return user;
+    }
+
+    public userClone entityToCLone(User user){
+        userClone userClone = new userClone();
+        return userClone;
     }
 }
